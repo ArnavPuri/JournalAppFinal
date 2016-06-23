574a9e4850a9af6f8f83b08148e4222eaa5bce39
Arnav Puri <arnavanytime@gmail.com>
2016-06-23 12:50:32 +0530

    sending notification at a particular time of Day

--- app/src/main/java/in/teachcoder/journalappfinal/MainActivity.java
+++ app/src/main/java/in/teachcoder/journalappfinal/MainActivity.java
@@ -1,11 +1,15 @@
 package in.teachcoder.journalappfinal;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
+
+import java.util.Calendar;
 
 import in.teachcoder.journalappfinal.data.Constants;
 import in.teachcoder.journalappfinal.data.DBMethods;
@@ -20,11 +24,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         entriesList = (ListView) findViewById(R.id.entries_list);
         myDB = new DBMethods(this);
+        setReminder();
         displayListView();
+
 
     }
 
-    public void displayListView(){
+    public void displayListView() {
         myDB.open();
         Cursor cursor = myDB.getEntries();
         String[] from = new String[]{
@@ -38,9 +44,26 @@ public class MainActivity extends AppCompatActivity {
                 R.id.entry_date,
                 R.id.entry_content
         };
-        SimpleCursorAdapter adapter = new SimpleCursorAdapter(MainActivity.this,R.layout.row_layout,
-                cursor, from,to,0);
+        SimpleCursorAdapter adapter = new SimpleCursorAdapter(MainActivity.this, R.layout.row_layout,
+                cursor, from, to, 0);
         entriesList.setAdapter(adapter);
         myDB.close();
+    }
+
+    public void setReminder() {
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(Calendar.HOUR_OF_DAY, 12);
+        calendar.set(Calendar.MINUTE, 42);
+        calendar.set(Calendar.SECOND, 0);
+
+        Intent notificationMessage = new Intent(this, NotificationHelper.class);
+
+//This is alarm manager
+        PendingIntent pi = PendingIntent.getBroadcast(this, 0,
+                notificationMessage, PendingIntent.FLAG_UPDATE_CURRENT);
+        AlarmManager am = (AlarmManager) getSystemService(ALARM_SERVICE);
+        am.set(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), pi);
+        am.setRepeating(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(),
+                AlarmManager.INTERVAL_DAY, pi);
     }
 }
---
+++ app/src/main/java/in/teachcoder/journalappfinal/NotificationHelper.java
@@ -0,0 +1,37 @@
+package in.teachcoder.journalappfinal;
+
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v4.app.NotificationCompat;
+import android.util.Log;
+
+/**
+ * Created by Arnav on 23-Jun-16.
+ */
+public class NotificationHelper extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        showNotification(context);
+    }
+
+    private void showNotification(Context context) {
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0,
+                new Intent(context, MainActivity.class), 0);
+        Log.d("Notificationhelper","Notification Set");
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context)
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setContentTitle("How was your Day?")
+                .setContentText("Day becomes memories");
+        builder.setContentIntent(pendingIntent);
+        builder.setDefaults(Notification.DEFAULT_SOUND);
+        builder.setAutoCancel(true);
+        NotificationManager mNotificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+        mNotificationManager.notify(1, builder.build());
+
+
+    }
+}
